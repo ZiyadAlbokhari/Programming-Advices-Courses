732c62a72ce6b75661fff4cc8e9c2db403d74b4d
Ziyad Albokhari <ziyad.y.albokhari@gmail.com>
2026-06-06 16:51:22 +0300

    Update Problem #24 - Validate Age Between 18 and 45.cpp

--- Course 4/Problem #24 - Validate Age Between 18 and 45.cpp
+++ Course 4/Problem #24 - Validate Age Between 18 and 45.cpp
@@ -3,11 +3,32 @@
 
 using namespace std;
 
+short ReadAge()
+{
+	short Age;
 
+	cout << "Please enter your age?" << endl;
+	cin >> Age;
+
+	return Age;
+}
+
+bool ValidateNumberInRange(short Number, short From, short To)
+{
+	return (Number >= From && Number <= To);
+}
+
+void PrintResult(short Age)
+{
+	if (ValidateNumberInRange(Age, 18, 45))
+		cout << Age << " is a valid age" << endl;
+	else
+		cout << Age << " is invalid age" << endl;
+}
 
 int main()
 {
-
+	PrintResult(ReadAge());
 
 	return 0;
 }

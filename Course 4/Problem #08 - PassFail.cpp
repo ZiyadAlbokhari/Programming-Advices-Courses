279edfc0eabd4c649edeea2fd3211ff28b743ecf
Ziyad Albokhari <ziyad.y.albokhari@gmail.com>
2026-06-06 16:49:26 +0300

    Update Problem #08 - PassFail.cpp

--- Course 4/Problem #08 - PassFail.cpp
+++ Course 4/Problem #08 - PassFail.cpp
@@ -2,9 +2,37 @@
 
 using namespace std;
 
+enum enPassFail { Pass = 1, Fail = 2 };
+
+short ReadMark()
+{
+	short Mark = 0;
+
+	cout << "Please enter your mark?" << endl;
+	cin >> Mark;
+
+	return Mark;
+}
+
+enPassFail CheckMark(short Mark)
+{
+	if (Mark >= 50)
+		return enPassFail::Pass;
+	else
+		return enPassFail::Fail;
+}
+
+void PrintResults(short Mark)
+{
+	if (CheckMark(Mark) == enPassFail::Pass)
+		cout << "\nYou Passed" << endl;
+	else
+		cout << "\nYou Failed" << endl;
+}
+
 int main()
 {
-
+	PrintResults(ReadMark());
 
 	return 0;
 }
